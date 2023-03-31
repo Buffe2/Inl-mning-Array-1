8f0feb38e8376462ef8e25e96d1614b16b272733
GustavJohansson420 <gujo04002@utb.vaxjo.se>
2023-03-31 09:18:05 +0200

    Add project files.

---
+++ ConsoleApp43/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Inl_uppgift_1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+            string[] m = new string[5];
+            m[0] = "Jag gillar glass.";
+            m[1] = "Jag springer snabbt.";
+            m[2] = "Jag är dålig på matte.";
+            m[3] = "Jag gillar bilar.";
+            m[4] = "Jag är en människa.";     //Jag vet att man kan lägga in dem i början. Men det blir tydligare såhär.
+
+            for(int i = 0; i < m.GetLength(0); i++)
+            {
+                Console.Write(m[i] + "\n");
+            }
+        }
+        
+    }
+}
